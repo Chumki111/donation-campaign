4b0c616d5d5f0f5d57f8801b08f4a9f81ea8ce82
Chumki khatun <chumkikhatun370@gamil.com>
2023-09-27 20:28:44 +0600

    added my statistics page with pie chart

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,57 +1,77 @@
 
-import {ResponsiveContainer,PieChart,Pie,Cell} from 'recharts';
+import { PieChart, Pie, Cell, } from 'recharts';
 import { useLoaderData } from "react-router-dom";
 const Statistics = () => {
 
-    const donationItems = JSON.parse(localStorage.getItem('donations')) || []
-     const donatedItems = donationItems.length;
-     
+    const donationItems = JSON.parse(localStorage.getItem('donations')) 
+    const allDonationData = useLoaderData();
+    let reMainingTotal = 0;
+    let yourDonate;
 
-     const allDonationData = useLoaderData();
-      const allDonationDataLength = allDonationData.length;
-      console.log(typeof(donatedItems))
-
-      const remainingItems = allDonationDataLength - donatedItems
-
-      const donationPercentage = (donatedItems / allDonationDataLength) * 100;
-
-   
-const data = [
-    { donations: 'all donations', length:{remainingItems} },
-    { donations: 'donated item', length:{donatedItems}},
-    
-  ];
-
-  
-  const COLORS = ['#0088FE', '#00C49F',];
+    if(donationItems){
+      yourDonate = donationItems.length;
+      reMainingTotal = allDonationData.length - yourDonate;
+    } else{
+      reMainingTotal = allDonationData.length;
+    }
       
 
+      
+
+      const data = [
+        { name: 'Total Donation', value: reMainingTotal },
+        { name: 'Your Donate', value: yourDonate },
+        
+      ];
+const COLORS = ['#0088FE', '#00C49F',];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};   
+
     return (
-        <div>    
+        <div className='lg:mx-auto py-10 mx-auto md:w-[650px] md:h[650px]'>    
 
-<p>Donation Percentage: {donationPercentage}%</p>
-            <ResponsiveContainer width='100%' >
-                 <PieChart width={400} height={400}>
-                 <Pie
-                 data={data}
-                 dataKey='value'
-                 nameKey='name'
-                 cx = '50%'
-                 cy = '50%'
-                 
-                 outerRadius={80}
-                 fill='#FFBB28'
-                 label
-                 
-                 />
 
-{
-              data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index]} />
-              ))
-            }
-                 </PieChart>
-            </ResponsiveContainer>
+
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={150}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+            <div  className='flex gap-8'>
+                
+                  <div className='flex gap-3 justify-center items-center'>
+                  <p>Your Donation</p>
+                  <p className='h-3 w-24 rounded-md bg-[#00C49F]'></p>
+                  </div>
+                
+                <div className='flex gap-3 justify-center items-center'>
+                    <p>
+                      Total Donation
+                    </p>
+                    <p className='h-3 w-24 rounded-md bg-[#0088FE]'></p>
+                </div>
+            </div>
            </div>
     );
 };
